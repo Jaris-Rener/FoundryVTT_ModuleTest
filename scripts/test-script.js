279df8605577de6743729b726546756177792116
Jaris Rener <jaris@leagueofgeeks.com>
2024-06-25 11:43:12 +1000

    Create repository

---
+++ scripts/test-script.js
@@ -0,0 +1,9 @@
+console.log('Loaded test script! Hooray!')
+
+Hooks.on("init", function() {
+    console.log("Foundry init :)");
+  });
+  
+  Hooks.on("ready", function() {
+    console.log("Foundry ready!!!! :)))");
+  });
